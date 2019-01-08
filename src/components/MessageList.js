71cd437cf1614fba2a92952582c6bed55a18eec6
Ruslan Zvarunchik <ruslan.zvarunchik@creditexpress.ua>
2019-01-08 16:43:08 +0200

    auto scroll for message list

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -4,9 +4,19 @@ import Message from './Message'
 
 class MessageList extends React.Component {
 
+    componentWillUpdate()
+    {
+        const node = ReactDOM.findDOMNode(this);
+        this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+    }
+
     componentDidUpdate()
     {
-        const node = ReactDOM.findDOMNode(this)
+        if (this.shouldScrollToBottom)
+        {
+            const node = ReactDOM.findDOMNode(this);
+            node.scrollTop = node.scrollHeight;
+        }
     }
 
     render() {
